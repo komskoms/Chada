27f5899e165104b72bc4fcfb2f97e36b9b30b866
김재환 <woghks7859@gmail.com>
2022-02-11 22:04:37 +0900

    OZH2-138 fix :  [Not complete] fix function that APP request and OBD reply car data at the same time

--- CAN_SERVER/src/src.ino
+++ CAN_SERVER/src/src.ino
@@ -1,5 +1,8 @@
 #include "hpp/header.hpp"
-
+/*
+	int ledPin = 10;
+	volatile int state = LOW;
+*/
 /******************************************************************************************
 ** Function Name : setup
 ** Description	 : call me when the program starts
@@ -10,6 +13,11 @@ void setup() {
 	CANInit();
 	setMaskFilt();
 	HC06.begin(SERIAL_SPEED);
+	/*
+	pinMode(ledPin, INPUT_PULLUP);
+	digitalWrite(ledPin, LOW);
+	attachInterrupt(digitalPinToInterrupt(ledPin), sendRequestData, CHANGE);
+	*/
 }
 
 /******************************************************************************************
@@ -18,17 +26,26 @@ void setup() {
 *******************************************************************************************/
 void loop() {
 	HC06.begin(SERIAL_SPEED);
-	int send_data = 0;
-	
+	// 차량 데이터 앱에 송신하는 코드
+	// int send_data = 0;
+	// send_info(send_data);
+
+	//앱에서 OBD에 데이터를 요청하고, OBD에서 앱에 응답하는 코드
+	if (HC06.available() > 0) {
+		String hc06_buf = HC06.readStringUntil('\n');
+		int request_num = hc06_buf.toInt();
+		request_info(request_num);
+	}
+}
+/*
+void sendRequestData() {
+	state = !state;
 	if (HC06.available() > 0) {
 		String num = HC06.readStringUntil('\n');
 		int request_num = num.toInt();
 		HC06.println(request_num);
 		request_info(request_num);
 	}
-	
-	// if (CANInit) {
-	// 	send_info(send_data);
-	// 	delay(5000);
-	// }
+ 	state = false;
 }
+*/

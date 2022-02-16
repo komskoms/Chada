dbade781ba10af14581f4cd52a8ed54ab393a6b6
김재환 <woghks7859@gmail.com>
2022-02-16 12:30:33 +0900

    OZH2-141 feat : Flutter_fix layout

--- Flutter/obd_test/lib/main.dart
+++ Flutter/obd_test/lib/main.dart
@@ -48,153 +48,167 @@ class _buildBodyState extends State<_buildBody> {
       mainList(),
     ])));
   }
-}
 
-Widget mainUpper(BuildContext context) {
-  return Container(
-      child: Center(
-          child: Row(
-    children: [
-      Expanded(child: mainLeftside(context)),
-      Expanded(child: mainSimpleInfo(context)),
-    ],
-    mainAxisAlignment: MainAxisAlignment.center,
-  )));
-}
-//#6161F5
-Widget mainLeftside(BuildContext context) {
-  return Row(
-    children: [
-      Container(
-        child: ElevatedButton(
-          child: Icon(Icons.menu),
-          onPressed: () => showMenu(
-              context: context,
-              position: RelativeRect.fromLTRB(50, 50, 50, 50),
-              items: [
-                PopupMenuItem<String>(
-                  child: Text("showSnackbar"),
-                  onTap: () {
-                    ScaffoldMessenger.of(context)
-                        .showSnackBar(SnackBar(content: Text("Wha~~~~~T??")));
-                  },
-                ),
-                PopupMenuItem<String>(
-                  child: Text("HUDmode"),
-                  onTap: () {
-                    // Navigator.pop(context);
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => HUDdisplay()));
-                  },
-                ),
-                PopupMenuItem<String>(
-                  child: Text("Setting"),
-                  onTap: () {
-                    // Navigator.pop(context);
-                    Navigator.push<void>(context,
-                        MaterialPageRoute(builder: (context) => HUDdisplay()));
-                  },
-                ),
-              ]),
-          // onPressed: () => ScaffoldMessenger.of(context)
-          //     .showSnackBar(SnackBar(content: Text("Wha~~~~~T??"))),
-          style: ButtonStyle(
-              minimumSize: MaterialStateProperty.all<Size>(Size(50, 60)),
-              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(25)))),
-        ),
-        margin: EdgeInsets.only(top: 10, bottom: 10, left: 30),
-      ),
-      Expanded(
-          child: Column(
-        children: [mainCarStatus(context), mainSelector(context)],
-      ))
-    ],
-    crossAxisAlignment: CrossAxisAlignment.start,
-    mainAxisAlignment: MainAxisAlignment.center,
-    mainAxisSize: MainAxisSize.max,
-  );
-}
+  Widget mainUpper(BuildContext context) {
+    return Container(
+        child: Center(
+            child: Row(
+      children: [
+        Expanded(child: mainLeftside(context)),
+        Expanded(child: mainSimpleInfo(context)),
+      ],
+      mainAxisAlignment: MainAxisAlignment.center,
+    )));
+  }
 
-Widget mainCarStatus(BuildContext context) {
-  return Container(
-      child: Column(
-    children: [
-      Container(
-        child: Text(
-          "My Car Status",
-          style: TextStyle(fontSize: 10),
-        ),
-      ),
-      Container(
-        child: Icon(
-          Icons.face_sharp,
-          size: 60,
-        ),
-        margin: EdgeInsets.all(5),
-      ),
-    ],
-  ));
-}
-
-Widget mainSelector(BuildContext context) {
-  return Container(
-    child: Column(
+  Widget mainLeftside(BuildContext context) {
+    return Row(
       children: [
         Container(
+          child: ElevatedButton(
+            child: Icon(
+              Icons.menu,
+              color: Color(0xffefefef),
+            ),
+            onPressed: () => showMenu(
+                context: context,
+                position: RelativeRect.fromLTRB(0, 120, 0, 100),
+                items: [
+                  PopupMenuItem<String>(
+                    child: Text("showSnackbar"),
+                    onTap: () {
+                      ScaffoldMessenger.of(context)
+                          .showSnackBar(SnackBar(content: Text("Wha~~~~~T??")));
+                    },
+                  ),
+                  PopupMenuItem<String>(
+                    child: Text("HUDmode"),
+                    onTap: () {
+                      // Navigator.pop(context);
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => HUDdisplay()));
+                    },
+                  ),
+                  PopupMenuItem<String>(
+                    child: Text("Setting"),
+                    onTap: () {
+                      // Navigator.pop(context);
+                      Navigator.push<void>(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => HUDdisplay()));
+                    },
+                  ),
+                ]),
+            // onPressed: () => ScaffoldMessenger.of(context)
+            //     .showSnackBar(SnackBar(content: Text("Wha~~~~~T??"))),
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Color(0xff6161F5)),
+                minimumSize: MaterialStateProperty.all<Size>(Size(50, 60)),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25)))),
+          ),
+          margin: EdgeInsets.only(top: 30, left: 30),
+        ),
+        Expanded(
+            child: Column(
+          children: [mainCarStatus(context), mainSelector(context)],
+        ))
+      ],
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.max,
+    );
+  }
+
+  Widget mainCarStatus(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Container(
+            child: Text(
+              "My Car Status",
+              style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+            ),
+            margin: EdgeInsets.only(top: 30),
+          ),
+          Container(
+            child: Icon(
+              Icons.face_sharp,
+              size: 60,
+            ),
+            margin: EdgeInsets.all(10),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget mainSelector(BuildContext context) {
+    return Container(
+      width: 80,
+      height: 100,
+      padding: EdgeInsets.only(left: 10, right: 10, top: 10),
+      child: Column(
+        children: [
+          Container(
+            child: Text(
+              "Selected",
+              style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+            ),
+            margin: EdgeInsets.only(bottom: 7),
+          ),
+          Image.asset('assets/images/1_speed.png', fit: BoxFit.contain),
+        ],
+        mainAxisSize: MainAxisSize.max,
+      ),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10), color: Color(0xff6161F5)),
+    );
+  }
+
+  Widget mainSimpleInfo(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          infoKeyValue("RPM", "750"),
+          infoKeyValue("Temp", "24'C"),
+          infoKeyValue("Load", "0%"),
+          infoKeyValue("Fuel", "42%"),
+        ],
+      ),
+      margin: EdgeInsets.only(top: 30, right: 30),
+      //padding: EdgeInsets.only(top: 50, bottom: 50),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10), color: Color(0xff6161F5)),
+    );
+  }
+
+  Widget infoKeyValue(String _key, String _val) {
+    return Row(
+      children: [
+        Expanded(
+            child: Container(
           child: Text(
-            "Selected",
+            _key,
             style: TextStyle(fontSize: 10),
           ),
-          margin: EdgeInsets.only(top: 5),
-        ),
-        Icon(Icons.precision_manufacturing_outlined, size: 60),
+          alignment: AlignmentDirectional(0.0, 0.0),
+          //margin: EdgeInsets.only(top: 10),
+          //padding: EdgeInsets.all(10),
+        )),
+        Expanded(
+            child: Container(
+          child: Text(
+            _val,
+            style: TextStyle(fontSize: 20),
+            textAlign: TextAlign.start,
+          ),
+        )),
       ],
-      mainAxisSize: MainAxisSize.max,
-    ),
-    decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10), color: Colors.indigo),
-  );
-}
-
-Widget mainSimpleInfo(BuildContext context) {
-  return Container(
-    child: Column(
-      children: [
-        infoKeyValue("RPM", "750"),
-        infoKeyValue("Temp", "24'C"),
-        infoKeyValue("Load", "0%"),
-        infoKeyValue("Fuel", "42%"),
-      ],
-    ),
-    margin: EdgeInsets.all(10),
-    decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10), color: Colors.indigo),
-  );
-}
-
-Widget infoKeyValue(String _key, String _val) {
-  return Row(
-    children: [
-      Expanded(
-          child: Container(
-        child: Text(
-          _key,
-          style: TextStyle(fontSize: 10),
-        ),
-        alignment: AlignmentDirectional(-1.0, 1.0),
-        margin: EdgeInsets.only(left: 15, top: 8),
-      )),
-      Expanded(
-          child: Container(
-        child: Text(
-          _val,
-          style: TextStyle(fontSize: 20),
-          textAlign: TextAlign.start,
-        ),
-        margin: EdgeInsets.all(10),
-      )),
-    ],
-  );
+    );
+  }
 }

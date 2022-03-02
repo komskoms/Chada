d95b8e08241f4c67a0d3b6906ce2666896d2e314
오승은 <tmddms927@gmail.com>
2022-03-02 20:42:00 +0900

    fix: fixed src.ino remove while() in SerialInit

--- CAN_SERVER/src/cpp/init.cpp
+++ CAN_SERVER/src/cpp/init.cpp
@@ -6,8 +6,6 @@
 *******************************************************************************************/
 void SerialInit() {
 	Serial.begin(SERIAL_SPEED);
-	while (!Serial)
-		delay(1000);
 	Serial.println("Serial Init ok!");
 	Serial.println("--------------------------------");
 }
